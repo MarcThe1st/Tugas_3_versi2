0f9f1728c6519de62ee1bc3c550233336b570045
MarcThe1st <113324828+MarcThe1st@users.noreply.github.com>
2022-09-26 05:14:42 +0700

    Initial commit

---
+++ app/src/main/java/com/example/tugas_3/Successful_Reset_Password.java
@@ -0,0 +1,14 @@
+package com.example.tugas_3;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Successful_Reset_Password extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_successful_reset_password);
+    }
+}
